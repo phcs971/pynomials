af05e7fb6ba16064e8bc81b14ba8b3d746a973de
phcs971 <48731731+phcs971@users.noreply.github.com>
2019-09-19 18:55:24 -0300

    Update pynomials.py

--- pynomials.py
+++ pynomials.py
@@ -1,5 +1,93 @@
-#from matplotlib.pyplot import figure, plot, show
-#from numpy import arange
+
+def poliAdd(poli1, poli2):
+    if poli1.var == poli2.var:
+        result = poli(poli1.var, poli1.name + ' + ' + poli2.name)
+        exponents1 = poli1.getExponents()
+        for exp in exponents1:
+            result.add((poli1.poli[exp], exp))
+        exponents2 = poli2.getExponents()
+        for exp in exponents2:
+            result.add((poli2.poli[exp], exp))
+        return result
+    else:
+        print("Can't sum different variabel polynomials")
+        
+def poliSub(poli1, poli2):
+    if poli1.var == poli2.var:
+        result = poli(poli1.var, poli1.name + ' - ' + poli2.name)
+        exponents1 = poli1.getExponents()
+        for exp in exponents1:
+            result.add((poli1.poli[exp], exp))
+        exponents2 = poli2.getExponents()
+        for exp in exponents2:
+            result.add((-poli2.poli[exp], exp))
+        return result
+    else:
+        print("Can't subtract different variabel polynomials")
+
+def poliMult(poli1, poli2):
+    if poli1.var == poli2.var:
+        result = poli(poli1.var, poli1.name + ' * ' + poli2.name)
+        exponents1 = poli1.getExponents()
+        exponents2 = poli2.getExponents()
+        for exp1 in exponents1:
+            for exp2 in exponents2: 
+                result.add((poli1.poli[exp1]*poli2.poli[exp2], exp1+exp2))
+        return result
+    else:
+        print("Can't multiply different variabel polynomials")
+
+def poliDiv(poli1, poli2):
+    if poli1.var == poli2.var:
+        result = poli(poli1.var, poli1.name + ' / ' + poli2.name)
+        exponents1 = poli1.getExponents()
+        exponents2 = poli2.getExponents()
+        maxExp1 = max(exponents1)
+        maxExp2 = max(exponents2)
+        p1 = poli1
+        p2 = poli2
+        while maxExp1 >= maxExp2:
+            coef = p1.poli[maxExp1]/p2.poli[maxExp2]
+            exp = maxExp1-maxExp2
+            p0 = poli(poli1.var, 'p0')
+            p0.add((coef, exp))
+            result.add((coef, exp))
+            p0 = poliMult(p2, p0)
+            p1 = poliSub(p1, p0)
+            exponents1 = p1.getExponents()
+            maxExp1 = max(exponents1)
+        remainder = p1
+        remainder.name = 'Remainder(' + poli1.name + ' / ' + poli2.name + ')'
+        remainder.clean()
+        return result, remainder
+    else:
+        print("Can't divide different variabel polynomials")
+
+def BRDiv(Poli, a):
+    exponents = Poli.getExponents()
+    coefs = []
+    result = poli(Poli.var, Poli.name + '/({}-('.format(Poli.var) + str(a) + '))')
+    for exp in exponents:
+        coefs.append(Poli.poli[exp])
+    remainder = 0
+    maxExp = max(exponents) - 1
+    for i in coefs:
+        remainder = remainder*a + i
+        result.add((remainder, maxExp))
+        maxExp -= 1
+    return result, remainder
+
+def intDivisors(x):
+    divs = []
+    maxdiv = int(x**0.5 + 1)
+    for i in range(1, maxdiv):
+        if x % i == 0:
+            if i != x/i:
+                divs.append(i)
+                divs.append(x/i)
+            else: 
+                divs.append(x)
+    return sorted(divs)
 
 class poli:
     def __init__(self, var = 'x', name = 'y'):
@@ -17,15 +105,22 @@ class poli:
         for exp in exponents:
             if self.poli[exp] == 0:
                 del self.poli[exp]
+        try:
+            if self.poli[self.c] == self.c:
+                del self.poli[self.c]
+        except:
+            pass
+        if self.poli == {}:
+            self.poli = {0: 0}
     
-    def getExponents(self):
+    def getExponents(self, reverse=True):
         try:
             if self.poli[self.c] == self.c:
                 exponents = list(self.poli.keys())
                 exponents.remove(self.c)
-                exponents = sorted(exponents, reverse=True)
+                exponents = sorted(exponents, reverse=reverse)
         except:
-            exponents = sorted(self.poli.keys(), reverse=True)
+            exponents = sorted(self.poli.keys(), reverse=reverse)
         return exponents
 
     def add(self, *args):
@@ -48,12 +143,41 @@ class poli:
         self.clean()
 
     def of(self, x):
+        self.clean()
         result = 0
         exponents = self.getExponents()
         for exp in exponents:
             val = self.poli[exp]*x**exp
             result += val
         return result
+
+    def intRoots(self):
+        try:
+            possibleDivs = intDivisors(self.poli[0])
+        except:
+            possibleDivs = [0]
+        roots = []
+        poli = self
+        while True:
+            exponents = poli.getExponents()
+            if len(exponents) == 0:
+                break
+            oldpoli = poli
+            for i in possibleDivs:
+                newpoli, rpos = BRDiv(poli, i)
+                if rpos == 0:
+                    roots.append(i)
+                    poli = newpoli
+                    continue
+                newpoli, rneg = BRDiv(poli, -i)
+                if rneg == 0:
+                    roots.append(-i)
+                    poli = newpoli
+                    continue
+            poli.show()
+            if poli == oldpoli :
+                break
+        return roots
 
     def derive(self, name=''):
         if name == '':
@@ -79,7 +203,7 @@ class poli:
         self.spoli.poli[self.c] = self.c
         return self.spoli
 
-    def area(self, start, end):
+    def defIntegrate(self, start, end):
         integral = self.integrate()
         return integral.of(end) - integral.of(start)
 
@@ -116,6 +240,8 @@ class poli:
                 if coef == 0:
                     writtenTerm = ''
                 writtenPoli += writtenTerm
+                if self.poli == {0: 0}:
+                    writtenPoli = ' 0'
             except:
                 pass
         try:
@@ -124,12 +250,3 @@ class poli:
         except:
             pass
         print(self.name + ' =' + writtenPoli)
-
-'''
-    def markerPlot(self, figSize, start, end, increment, color='black', marker='o'):
-        fig = figure(figsize=figSize)
-        for x in arange(start, end, increment):
-            y = self.of(x)
-            plot(x, y, color=color, marker=marker)
-        show()
-'''
