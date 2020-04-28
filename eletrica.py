dce1afb7b7f31bd52c3c8585a471849351b03509
Pedro <phcs.971@gmail.com>
2020-04-27 21:36:12 -0300

    Update eletrica.py

--- eletrica.py
+++ eletrica.py
@@ -19,14 +19,11 @@ lmbd = Var('lambda')
 Vbarra = k*lmbd*Ln((L + SquareRoot(L**2 + d**2))/d)
 Vbarra2 = k*q/L*Ln((L + SquareRoot(L**2 + d**2))/d)
 
+Vbarra_lateral = k*lmbd*Ln(1+ L/d)
+
 Vanel = k*q/(SquareRoot(z**2+R**2))
 
 Vdisco = sigma * (SquareRoot(z**2+R**2)-z) / (2*E)
 Vdisco2 = q / (pi*R**2) * (SquareRoot(z**2+R**2)-z) / (2*E)
 
-
-print(Vdisco2.value(
-    q=5e-9,
-    z=0.01,
-    R=17.5e-3/2
-)*(-e))
+print(Vbarra_lateral.value(lmbd=6e-11, L=0.12, d=0.04))
